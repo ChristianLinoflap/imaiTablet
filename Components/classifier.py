9e24f9e90359c728e58e5dfcbc111f6b9ad6d937
AquilinoLinoflapTech <angelo.aquilino@linoflaptech.com>
2024-03-05 10:19:50 +0800

    05/03/2024 Updated Classifier

--- Components/classifier.py
+++ Components/classifier.py
@@ -11,10 +11,11 @@ class ObjectClassifier:
     def __init__(self):
         pygame.mixer.init()
         pygame.display.set_caption('')
+        self.scan_sound = pygame.mixer.Sound("Assets\\scanned_item.mp3")
         self.fgbg = cv2.createBackgroundSubtractorMOG2()
         self.image_directory = "capture"
         self.frame_count = 0
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(1)
         self.create_directory(self.image_directory)
         self.stop_event = threading.Event()
         threading.Thread(target=self.run_classifier).start()
@@ -42,12 +43,20 @@ class ObjectClassifier:
         self.predicted_classes = []
         while not self.stop_event.is_set():
             ret, frame = self.cap.read()
-            cropped_frame = frame[150:1000, 90:500]
-            fgmask = self.fgbg.apply(cropped_frame)
-            _, fgmask = cv2.threshold(fgmask, 120, 255, cv2.THRESH_BINARY)
-            contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            files = []
+            # Adjusting the cropping region
 
+            top = 150   
+            bottom = 500
+            left = 50   
+            right = 420
+
+            # Cropping the frame with the new parameters
+            cropped_frame = frame[top:bottom, left:right]
+            # fgmask = self.fgbg.apply(cropped_frame)
+            # _, fgmask = cv2.threshold(fgmask, 120, 255, cv2.THRESH_BINARY)
+            # contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            files = []
+            # cropped_frame_resized = cv2.resize(cropped_frame, (256, 256))
             saturated_frame = self.saturate_image(cropped_frame)
             cv2.imwrite(f"capture/frame.png", saturated_frame)
             url = 'http://192.168.254.102:5000/upload2'
@@ -66,12 +75,13 @@ class ObjectClassifier:
                 if files:
                     try:
                         response = requests.post(url, files=files)
-                        print(response.text)
                         if '<' in response.text or 'pass' in response.text:
                             print("'< found in response text.")
                         else:
                             with open("predicted_class.txt", "w") as file:
                                 file.write(response.text)
+                            self.scan_sound.play()
+                            self.scan_sound.set_volume(1)
                     except Exception as e:
                         print(f"Error occurred: {e}")
                 else:
@@ -80,8 +90,8 @@ class ObjectClassifier:
                 elapsed_time = end_time - start_time
                 print("Elapsed time: ", elapsed_time)
 
-            cv2.imshow('Original Frame', frame)
-            cv2.imshow('Motion Detection', fgmask)
+            cv2.imshow('Original Frame', cropped_frame)
+            # cv2.imshow('Motion Detection', fgmask)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 self.stop_classifier()
@@ -89,12 +99,12 @@ class ObjectClassifier:
 
     def pause_scanning(self):
         self.stop_event.set()
-        print("Pausing started.")
+        # print("Pausing started.")
 
     def resume_scanning(self):
         self.stop_event.clear()
         threading.Thread(target=self.run_classifier).start()
-        print("Scanning started.")
+        # print("Scanning started.")
 
     def stop_classifier(self):
         try:
